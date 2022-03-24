e144fa6904ead51ba928ff91b2a87d326c5e7d4c
Brilianti Puspita <90308452+briliantipuspita@users.noreply.github.com>
2022-03-25 06:18:15 +0700

    Modular Aritmethic

---
+++ ModArit1.py
@@ -0,0 +1,4 @@
+def modularArithmetic(a, m):
+    return (a % m)
+
+print(min(modularArithmetic(11,6),modularArithmetic(8146798528947,17)))
---
+++ ModArit2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from math import gcd
+
+a = 273246787654
+p = 65537
+
+if gcd(a,p)==1: 
+        print(1)
