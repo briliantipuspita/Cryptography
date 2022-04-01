b28c6f37a8ad086157d15eaa133e6745c3fc9357
Brilianti Puspita <90308452+briliantipuspita@users.noreply.github.com>
2022-04-01 21:11:07 +0700

    Symmetric Ciphers Course

---
+++ symetric ciphers/aes_decrypt.py
@@ -0,0 +1,24 @@
+def decrypt(key, ciphertext):
+    round_keys = expand_key(key) # Remember to start from the last round key and work backwards through them when decrypting
+
+   
+    state = bytes2matrix(ciphertext)
+
+   
+    state = add_round_key(state, round_keys[-1])
+
+    for i in range(N_ROUNDS - 1, 0, -1):
+        inv_shift_rows(state)
+        inv_sub_bytes(state)
+        state = add_round_key(state, round_keys[i])
+        inv_mix_columns(state)
+
+
+    inv_shift_rows(state)
+    inv_sub_bytes(state)
+    state = add_round_key(state, round_keys[0])
+
+    
+    plaintext = matrix2bytes(state)
+
+    return plaintext
