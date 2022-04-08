09e90d78edaff3c16259967f764be8c51871a7ad
Brilianti Puspita <90308452+briliantipuspita@users.noreply.github.com>
2022-04-08 19:33:52 +0700

    symmetric Starter  & Block Cipher course

---
+++ symmetric Starter  & Block Cipher/Modes-of-operation_starter.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env python
+import requests
+
+BASE_URL = "http://aes.cryptohack.org/block_cipher_starter"
+
+r = requests.get(f"{BASE_URL}/encrypt_flag")
+data = r.json()
+ciphertext = data["ciphertext"]
+print("ciphertext", ciphertext)
+
+r = requests.get(f"{BASE_URL}/decrypt/{ciphertext}")
+data = r.json()
+plaintext = data["plaintext"]
+print("plaintext", plaintext)
+
+print("flag", bytearray.fromhex(plaintext).decode())
+
+# flag = crypto{bl0ck_c1ph3r5_4r3_f457_!}
